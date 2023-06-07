2250f39cb14d1223048b9bc5c64d85d0f90296f5
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-06-07 12:04:41 -0500

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Justin Hold on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
